2b32208643cd2c35b3ca025bfc8fa0350b06ad7e
Zach Evans <zqevans@Zachs-MacBook-Pro.local>
2022-04-17 10:33:25 -0600

    Adding tests

--- test/modules.test.py
+++ test/modules.test.py
@@ -1,8 +1,15 @@
 from utils import MidSideEncoding, MidSideDecoding
+import torch
 
 def testMidSideEncoding():
     #make a noise signal
-    #MS encode
-    #MS decode
-    #asset equality
-    pass
+    noise = torch.randn([4, 2, 131072], device='cuda')
+    encoded_noise = MidSideEncoding()(noise)
+    decoded_noise = MidSideDecoding()(encoded_noise) 
+    assert(torch.equal(noise, decoded_noise))
+
+def runTests():
+    testMidSideEncoding()
+
+if __name__ == '__main__':
+    runTests()
