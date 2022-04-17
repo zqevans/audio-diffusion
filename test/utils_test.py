b1930efc042c9487caa3eb7a2a3c8c1417e2d504
Zach Evans <zqevans@Zachs-MacBook-Pro.local>
2022-04-17 11:04:20 -0600

    Adding another mid-side test

--- test/utils_test.py
+++ test/utils_test.py
@@ -5,13 +5,25 @@ import unittest
 
 class TestUtils(unittest.TestCase):
 
-    def test_mid_side_encoding(self):
-        noise = torch.randn([2, 131072], device='cuda')
+    def test_mid_side_encoding_invertible(self):
+        signal = torch.randn([2, 131072], device='cuda')
         encoder = MidSideEncoding()
         decoder = MidSideDecoding()
 
         #Encoding and decoding should be a no-op
-        assert(torch.equal(noise, decoder(encoder(noise))))
+        assert(torch.equal(signal, decoder(encoder(signal))))
+
+    def test_mid_side_encoding_mono(self):
+        mono_signal = torch.randn([1, 131072], device='cuda')
+        # Make the signal mono
+        signal = mono_signal.repeat(2, 1)
+        encoder = MidSideEncoding()
+        encoded_mono = encoder(signal)
+
+        #Side content should be all zeros
+        zeros = torch.zeros_like(encoded_mono[1])
+        assert(torch.equal(zeros, encoded_mono[1]))
+
 
 if __name__ == '__main__':
     unittest.main()
