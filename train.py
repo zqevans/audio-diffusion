8f831d748123c98fdb67054f9e803a65467edf36
Zach Evans <zqevans@gmail.com>
2022-04-27 21:09:11 -0700

    Adding back global step for demo-every

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ class DemoCallback(pl.Callback):
     @rank_zero_only
     @torch.no_grad()
     def on_train_batch_end(self, trainer, module, outputs, batch, batch_idx, unused=0):
-        if (batch_idx - 1) % self.demo_every != 0:
+        if (trainer.global_step - 1) % self.demo_every != 0:
             return
 
         # noise = torch.zeros([4, 2, self.demo_samples])
