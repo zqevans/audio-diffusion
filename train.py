3653b6ae3301d328364af70e0e46d8800fd5fd87
Zach Evans <zqevans@gmail.com>
2022-05-08 15:24:57 -0700

    Change default sample size

--- train.py
+++ train.py
@@ -115,8 +115,8 @@ def main():
                    help='number of GPUs to use for training')
     p.add_argument('--pqmf-bands', type=int, default=8,
                    help='number of sub-bands for the PQMF filter')
-    p.add_argument('--sample-size', type=int, default=65536,
-                   help='Number of samples to train on, must be a multiple of 65536')
+    p.add_argument('--sample-size', type=int, default=16384,
+                   help='Number of samples to train on, must be a multiple of 16384')
     p.add_argument('--demo-every', type=int, default=1000,
                    help='Number of steps between demos')
     p.add_argument('--demo-steps', type=int, default=500,
